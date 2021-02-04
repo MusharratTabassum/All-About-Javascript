429878d656bfaaa70627971db8aef7d86fc562d0
MusharratTabassum <39344758+MusharratTabassum@users.noreply.github.com>
2021-02-04 22:41:17 +0600

    Add files via upload

---
+++ 01basics/nullandbull.js
@@ -0,0 +1,13 @@
+var temp = 0
+
+console.log("Current Temperature is : " + temp)
+
+let grade = true
+
+console.log(grade)
+
+let actualMarks = 7
+
+let myGrade = (actualMarks < 10)
+
+console.log(myGrade) 
---
+++ 01basics/variables.js
@@ -0,0 +1,14 @@
+let name = 'john'
+
+let score = 100.0
+let bonus = 20
+
+let totalScore = score+bonus
+
+console.log(totalScore)
+
+let firstName = 'john'
+firstName = 'jane'
+let lastName = 'Doe'
+
+console.log(firstName + ' '+lastName)
