54ec0c0e49d4cb0843bde9adb21ffb648045b197
MusharratTabassum <39344758+MusharratTabassum@users.noreply.github.com>
2021-02-04 22:42:48 +0600

    added basic

---
+++ 02basics/learnifElse.js
@@ -0,0 +1,9 @@
+
+if(8>17){
+    console.log('I am inside if Block')
+    console.log('I am still inside if Block')
+}else if(false === false){
+    console.log('I am inside IF IN CAPS BLock')
+}else{
+    console.log('NOTICE THIS PART')
+}
---
+++ 02basics/marvel.js
@@ -0,0 +1,36 @@
+// const superHero = ['Iron Man','Spiderman','Capt. America']
+
+
+// console.log(superHero); 
+// console.log(superHero[0]);
+// console.log(superHero[2]);
+// console.log(superHero[superHero.length-1]);
+
+
+// console.log(`We have ${superHero.length} super heroes`);
+
+
+const numbers = ['one','two','three','four','five','six']
+
+
+//Start
+// console.log(numbers.shift());
+// console.log(numbers);
+
+// numbers.unshift('Somethings')
+// console.log(numbers);
+
+//End
+
+// console.log('The element to be deleted is: '+numbers.pop());
+// console.log(numbers);
+
+
+// numbers.push('seven')
+// console.log(numbers);
+
+//Middle
+numbers.splice(2,1,'Something')
+console.log(numbers);
+
+
